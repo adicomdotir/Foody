b90a794791fa4adee00e59604d434c26400377a1
adicomdotir <adicom.ir@gmail.com>
2022-10-09 21:50:44 +0330

    Add room db requirement

---
+++ app/src/main/java/ir/adicom/foody/data/database/RecipesEntity.kt
@@ -0,0 +1,14 @@
+package ir.adicom.foody.data.database
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import ir.adicom.foody.models.FoodRecipe
+import ir.adicom.foody.util.Constants.Companion.RECIPES_TABLE
+
+@Entity(tableName = RECIPES_TABLE)
+class RecipesEntity(
+    var foodRecipe: FoodRecipe
+) {
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
+}
