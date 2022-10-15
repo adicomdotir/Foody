ee77580f0b31adc305f1aaed4f4497024db6d4ac
adicomdotir <adicom.ir@gmail.com>
2022-10-15 22:22:17 +0330

    Complete InstructionsFragment screen

--- app/src/main/java/ir/adicom/foody/ui/fragments/instructions/InstructionsFragment.kt
+++ app/src/main/java/ir/adicom/foody/ui/fragments/instructions/InstructionsFragment.kt
@@ -5,14 +5,27 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import ir.adicom.foody.R
+import ir.adicom.foody.models.Result
+import ir.adicom.foody.util.Constants
 
 class InstructionsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instructions, container, false)
+        val view = inflater.inflate(R.layout.fragment_instructions, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable(Constants.RECIPE_RESULT_KEY)
+
+        val instructionsWebview = view.findViewById<WebView>(R.id.instructions_webView)
+        instructionsWebview.webViewClient = object : WebViewClient() {}
+        val websiteUrl = myBundle!!.sourceUrl
+        instructionsWebview.loadUrl(websiteUrl)
+
+        return view
     }
 }
